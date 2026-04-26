2bb94c12f882e2ab9969e51df71029ffeddcb85b
NitinVett <76098798+NitinVett@users.noreply.github.com>
2026-04-25 22:23:05 -0400

    fixed refresh

--- Assets/Scripts/RefreshButton.cs
+++ Assets/Scripts/RefreshButton.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class RefreshButton : MonoBehaviour
 {
+    public Character character;
     public void Refresh()
     {
         Transform shopButtons = transform.parent.Find("ShopButtons");
-
+        character.money -= 10;
         foreach (Transform child in shopButtons)
         {
             child.gameObject.SetActive(true);
